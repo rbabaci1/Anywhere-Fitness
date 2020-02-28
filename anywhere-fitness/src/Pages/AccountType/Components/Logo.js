2f1f96f3f3de9349b139066ee068367715e0ad76
rabah babaci <4rabah@gmail.com>
2020-02-27 20:11:58 -0800

    create the styled LogoContainer

--- anywhere-fitness/src/Pages/AccountType/Components/Logo.js
+++ anywhere-fitness/src/Pages/AccountType/Components/Logo.js
@@ -2,6 +2,37 @@ import React from 'react';
 
 import styled from 'styled-components';
 
+const LogoContainer = styled.div`
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  h1 {
+    font-family: Ubuntu;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 34px;
+    line-height: 39px;
+    text-align: center;
+    letter-spacing: 0.36px;
+    color: #fff;
+
+    span {
+      color: red;
+    }
+  }
+`;
+
 export default function Logo() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>
+        Anywhere
+        <br />
+        <span>Fitness</span>
+      </h1>
+    </div>
+  );
 }
