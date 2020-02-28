92a261daec8a2ef250ddbd6799810cd86ad81725
rabah babaci <4rabah@gmail.com>
2020-02-27 21:12:38 -0800

    finish styling the main content components

--- anywhere-fitness/src/Pages/AccountType/Components/AccountTypeHome.js
+++ anywhere-fitness/src/Pages/AccountType/Components/AccountTypeHome.js
@@ -7,6 +7,21 @@ import instructorImage from '../../Images/instructorImage.png';
 
 const MainContent = styled.div`
   display: flex;
+  justify-content: space-evenly;
+  margin: 50px;
+
+  div {
+    h2 {
+      font-style: normal;
+      font-weight: bold;
+      font-size: 22px;
+      line-height: 25px;
+      text-align: center;
+      letter-spacing: 0.36px;
+      color: #f7f7f7;
+      margin-bottom: 5px;
+    }
+  }
 `;
 
 export default function AccountTypeHome() {
@@ -14,17 +29,17 @@ export default function AccountTypeHome() {
     <div>
       <Header />
 
-      <div>
+      <MainContent>
         <div>
           <h2>Client</h2>
           <img src={clientImage} alt='client athlete'></img>
         </div>
 
         <div>
-          <h2>Instructor</h2>
+          <h2 style={{ color: '#CA1F3D' }}>Instructor</h2>
           <img src={instructorImage} alt='instructor athlete'></img>
         </div>
-      </div>
+      </MainContent>
     </div>
   );
 }
