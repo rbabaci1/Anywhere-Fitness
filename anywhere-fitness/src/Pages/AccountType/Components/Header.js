82ee7486365270fb5e8340f83aae18b275314078
rabah babaci <4rabah@gmail.com>
2020-02-27 20:43:00 -0800

    add style to the descreption

--- anywhere-fitness/src/Pages/AccountType/Components/Header.js
+++ anywhere-fitness/src/Pages/AccountType/Components/Header.js
@@ -3,10 +3,30 @@ import React from 'react';
 import Logo from './Logo';
 import styled from 'styled-components';
 
+const Description = styled.div`
+  display: flex;
+  justify-content: center;
+
+  h3 {
+    font-family: Ubuntu;
+    font-style: normal;
+    width: 343px;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 23px;
+    text-align: center;
+    letter-spacing: 0.36px;
+    color: #f7f7f7;
+  }
+`;
+
 export default function Header() {
   return (
     <div>
       <Logo />
+      <Description>
+        <h3>Choose if you want to join as a Client or an Instructor</h3>
+      </Description>
     </div>
   );
 }
--- anywhere-fitness/src/Pages/AccountType/Components/Logo.js
+++ anywhere-fitness/src/Pages/AccountType/Components/Logo.js
@@ -3,12 +3,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 const LogoContainer = styled.div`
-  position: absolute;
-  left: 0%;
-  right: 0%;
-  top: 0%;
-  bottom: 0%;
-
   h1 {
     font-family: Ubuntu;
     font-style: normal;
@@ -17,7 +11,7 @@ const LogoContainer = styled.div`
     line-height: 39px;
     text-align: center;
     letter-spacing: 0.36px;
-    color: #fff;
+    color: #f7f7f7;
 
     span {
       color: #ca1f3d;
