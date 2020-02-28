cd4ace5e017f2d47f3008a366e65170c310303b9
Brownbatz <Lopeztgabriel4@gmail.com>
2020-02-27 19:59:41 -0700

    added basic comment layout to the app file

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -5,7 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      {/*Login/Signup Page*/}
+      {/*Choose account type page*/}
+      {/*User home page*/}
+      {/*Class Page / schedule /// reserve button is on schedule next to the class
+      and the about class button is a feature we will add specifically for the web app*/}
+      {/*Invite a friend*/}
     </div>
   );
 }
