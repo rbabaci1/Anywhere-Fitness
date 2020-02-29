408840d4d106f809432b1dac87e6ce0ff6f72b3e
rabah babaci <4rabah@gmail.com>
2020-02-29 09:18:51 -0800

    render the Logo and the text under it in LoginPage

--- anywhere-fitness/src/Pages/LoginPage/LoginPage.js
+++ anywhere-fitness/src/Pages/LoginPage/LoginPage.js
@@ -4,10 +4,27 @@ import styled from 'styled-components';
 import Logo from '../../ReusableComponents/Logo';
 import Button from '../../ReusableComponents/Button';
 
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  p {
+    width: 344px;
+    font-size: 20px;
+    line-height: 23px;
+    text-align: center;
+    letter-spacing: 0.36px;
+    color: #f7f7f7;
+  }
+`;
+
 export default function LoginPage() {
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <LoginContainer>
+      <Logo />
+
+      <p>Log in by providing the information below</p>
+    </LoginContainer>
   );
 }
