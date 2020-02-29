1d7ef6c09690b950ea09657633f778171442cac7
rabah babaci <4rabah@gmail.com>
2020-02-28 17:08:48 -0800

    import and render ClientType in ClientSelected

--- anywhere-fitness/src/Pages/AccountType/Components/ClientSelected.js
+++ anywhere-fitness/src/Pages/AccountType/Components/ClientSelected.js
@@ -1,10 +1,14 @@
 import React from 'react';
+
 import Header from './Header';
+import ClientType from './ClientType';
 
 export default function ClientSelected() {
   return (
     <div>
       <Header />
+
+      <ClientType />
     </div>
   );
 }
