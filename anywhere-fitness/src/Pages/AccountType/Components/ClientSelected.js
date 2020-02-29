e9a6ab46e0fdee4f22d4ef31b50d7de4a195696a
rabah babaci <4rabah@gmail.com>
2020-02-28 17:23:08 -0800

    add Style to ClientSelected component

--- anywhere-fitness/src/Pages/AccountType/Components/ClientSelected.js
+++ anywhere-fitness/src/Pages/AccountType/Components/ClientSelected.js
@@ -4,16 +4,33 @@ import styled from 'styled-components';
 import Header from './Header';
 import ClientType from './ClientType';
 
+const ClientSelectDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  p {
+    width: 341px;
+    color: #f7f7f7;
+    font-size: 17px;
+    line-height: 20px;
+    text-align: center;
+    letter-spacing: 0.36px;
+  }
+`;
+
 export default function ClientSelected() {
   return (
     <div>
       <Header />
       <ClientType />
 
-      <p>
-        As a client, you will be able to search and sign up for fitness classes
-        based on location, skill level, and preferred work out type.
-      </p>
+      <ClientSelectDiv>
+        <p>
+          As a client, you will be able to search and sign up for fitness
+          classes based on location, skill level, and preferred work out type.
+        </p>
+      </ClientSelectDiv>
     </div>
   );
 }
