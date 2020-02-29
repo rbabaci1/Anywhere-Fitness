21ad45d414e5d8b451de52cf71d6ddca36a385bd
rabah babaci <4rabah@gmail.com>
2020-02-29 09:13:22 -0800

    import Logo and Button to LandingPage

--- anywhere-fitness/src/Pages/LoginPage/LoginPage.js
+++ anywhere-fitness/src/Pages/LoginPage/LoginPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import Logo from '../../ReusableComponents/Logo';
+import Button from '../../ReusableComponents/Button';
+
 export default function LoginPage() {
   return (
     <div>
