be0652585655e9c023706c2a7491bd877c388569
rabah babaci <4rabah@gmail.com>
2020-02-28 17:06:20 -0800

    import and render Header in ClientSelected

--- anywhere-fitness/src/Pages/AccountType/Components/ClientSelected.js
+++ anywhere-fitness/src/Pages/AccountType/Components/ClientSelected.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
 export default function ClientSelected() {
   return (
     <div>
-      <h1>Client</h1>
+      <Header />
     </div>
   );
 }
