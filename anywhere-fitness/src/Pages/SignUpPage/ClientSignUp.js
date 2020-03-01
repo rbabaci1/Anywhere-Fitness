2be08b625c70ed74ac5fcc597b9af7e2c73ce4c9
rabah babaci <4rabah@gmail.com>
2020-02-29 17:04:29 -0800

    import axios to clientSignUp

--- anywhere-fitness/src/Pages/SignUpPage/ClientSignUp.js
+++ anywhere-fitness/src/Pages/SignUpPage/ClientSignUp.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Form, Field, withFormik } from 'formik';
 import * as yup from 'yup';
+import axios from 'axios';
 
 export default function ClientSignUp() {
   return <div>ClientSignUp</div>;
