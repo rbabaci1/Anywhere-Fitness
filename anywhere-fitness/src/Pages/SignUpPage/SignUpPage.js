d3258e331d06c5649014432cf262aded12f10fbd
rabah babaci <4rabah@gmail.com>
2020-02-29 18:00:31 -0800

    create two more input Fields form email and password

--- anywhere-fitness/src/Pages/SignUpPage/SignUpPage.js
+++ anywhere-fitness/src/Pages/SignUpPage/SignUpPage.js
@@ -14,6 +14,18 @@ function SignUpPage({ match }) {
           Name
           <Field type='text' name='name' placeholder='Enter name' />
         </label>
+
+        <label>
+          Email
+          <Field type='email' name='email' placeholder='Enter email' />
+        </label>
+
+        <label>
+          Password
+          <Field type='password' name='password' placeholder='Enter password' />
+        </label>
+
+        <div></div>
       </Form>
     </div>
   );
