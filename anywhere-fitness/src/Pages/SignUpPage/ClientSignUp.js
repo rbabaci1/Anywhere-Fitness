7f59f5872964d7f669ca71fcb7bad7195b8ad27a
rabah babaci <4rabah@gmail.com>
2020-02-29 17:03:44 -0800

    import Form, Field, withFormik to ClientSignUp

--- anywhere-fitness/src/Pages/SignUpPage/ClientSignUp.js
+++ anywhere-fitness/src/Pages/SignUpPage/ClientSignUp.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Form, Field, withFormik } from 'formik';
 import * as yup from 'yup';
 
 export default function ClientSignUp() {
