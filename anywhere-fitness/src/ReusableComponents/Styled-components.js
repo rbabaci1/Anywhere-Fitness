50b6f3fc9d02b476117d6eb1c13ce8ca59160992
rabah babaci <4rabah@gmail.com>
2020-03-01 15:46:27 -0800

    reuse the FormContainer in MoreInfoForm

--- anywhere-fitness/src/ReusableComponents/Styled-components.js
+++ anywhere-fitness/src/ReusableComponents/Styled-components.js
@@ -1,1 +1,57 @@
 import styled from 'styled-components';
+
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  p {
+    width: 344px;
+    font-size: 22px;
+    line-height: 25px;
+    text-align: center;
+    letter-spacing: 0.36px;
+    color: #f7f7f7;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    width: 330px;
+    margin-top: 30px;
+
+    label {
+      display: flex;
+      flex-direction: column;
+      color: #f7f7f7;
+      font-size: 17px;
+      font-weight: normal;
+      line-height: 20px;
+      letter-spacing: 0.36px;
+      margin-bottom: 13px;
+
+      input {
+        border-radius: 3px;
+        box-sizing: border-box;
+        border: 2px solid #787777;
+        padding: 10px 6px;
+        font-size: 17px;
+        background: inherit;
+        color: #f7f7f7;
+        font-family: Ubuntu;
+      }
+
+      .select {
+        font-size: 20px;
+        height: 40px;
+        background: inherit;
+        border: 2px solid #787777;
+        color: #f7f7f7;
+      }
+    }
+    button {
+      align-self: center;
+      margin-top: 100px;
+    }
+  }
+`;
