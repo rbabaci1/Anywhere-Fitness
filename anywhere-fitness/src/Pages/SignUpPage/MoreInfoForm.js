e029eb4e55d980779002a85ea59b1d6acf36c172
rabah babaci <4rabah@gmail.com>
2020-03-01 10:56:39 -0800

    add style to MoreInfoForm

--- anywhere-fitness/src/Pages/SignUpPage/MoreInfoForm.js
+++ anywhere-fitness/src/Pages/SignUpPage/MoreInfoForm.js
@@ -4,9 +4,64 @@ import styled from 'styled-components';
 
 import Logo from '../../ReusableComponents/Logo';
 
+const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  p {
+    width: 344px;
+    font-size: 22px;
+    line-height: 25px;
+    text-align: center;
+    letter-spacing: 0.36px;
+    color: #f7f7f7;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    width: 330px;
+    margin-top: 30px;
+
+    label {
+      display: flex;
+      flex-direction: column;
+      color: #f7f7f7;
+      font-size: 17px;
+      font-weight: normal;
+      line-height: 20px;
+      letter-spacing: 0.36px;
+      margin-bottom: 13px;
+
+      input {
+        border-radius: 3px;
+        box-sizing: border-box;
+        border: 2px solid #787777;
+        padding: 10px 6px;
+        font-size: 17px;
+        background: inherit;
+        color: #f7f7f7;
+        font-family: Ubuntu;
+      }
+
+      .select {
+        font-size: 20px;
+        height: 40px;
+        background: inherit;
+        border: 2px solid #787777;
+        color: #f7f7f7;
+      }
+    }
+  }
+`;
+
 function MoreInfoForm() {
   return (
-    <div>
+    <FormContainer>
+      <Logo />
+      <p>Tell us more about you!</p>
+
       <Form>
         <label>
           Exercise you specialize in
@@ -31,7 +86,7 @@ function MoreInfoForm() {
           </Field>
         </label>
       </Form>
-    </div>
+    </FormContainer>
   );
 }
 
