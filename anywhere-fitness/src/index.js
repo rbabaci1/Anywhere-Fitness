8bb470010b26c4c1342f7e1cfe587e3a0da7ae9e
Brownbatz <Lopeztgabriel4@gmail.com>
2020-03-03 19:37:41 -0700

    New update

--- anywhere-fitness/src/index.js
+++ anywhere-fitness/src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
+const store = createStore(reducer);
+
+function reducer(){
+  return {
+    title: 'Hi from the Redux Store!'
+  }
+}
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App /> 
+    </Router>  
+  </Provider>,
 
   document.getElementById('root')
 );
