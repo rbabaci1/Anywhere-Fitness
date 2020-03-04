1adba2f1e52695e3fe8aaa4504dba4721a6ba84c
rabah babaci <4rabah@gmail.com>
2020-03-03 16:23:10 -0800

    import all icon images to HomePageNav

--- anywhere-fitness/src/ReusableComponents/HomePageNav.js
+++ anywhere-fitness/src/ReusableComponents/HomePageNav.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import accountIcon from '../Pages/Images/accountIcon.png';
+import searchIcon from '../Pages/Images/searchIcon.png';
+import calendarIcon from '../Pages/Images/calendarIcon.png';
+import settingIcon from '../Pages/Images/settingIcon.png';
+
 import Logo from './Logo';
 
 export default function HomePageNav() {
   return (
     <div>
-      <h1>Home page nav bar</h1>
+      <Logo />
+
+      <section>
+        <img src={accountIcon} />
+        <img src={searchIcon} />
+        <img src={calendarIcon} />
+        <img src={settingIcon} />
+      </section>
     </div>
   );
 }
