6b9d5c8d99c979490386c9ed76d3a87502032ad2
rabah babaci <4rabah@gmail.com>
2020-03-03 16:32:33 -0800

    import NavLing from react route dom

--- anywhere-fitness/src/ReusableComponents/HomePageNav.js
+++ anywhere-fitness/src/ReusableComponents/HomePageNav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 import accountIcon from '../Pages/Images/accountIcon.png';
 import searchIcon from '../Pages/Images/searchIcon.png';
