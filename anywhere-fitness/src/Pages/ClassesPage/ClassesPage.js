cbb1f975573a2cf2fab4da6b4713819e1fada4cb
rabah babaci <4rabah@gmail.com>
2020-03-03 20:51:13 -0800

    finish the styling for ClassCard

--- anywhere-fitness/src/Pages/ClassesPage/ClassesPage.js
+++ anywhere-fitness/src/Pages/ClassesPage/ClassesPage.js
@@ -14,10 +14,17 @@ const classData = {
 const ClassesContainer = styled.div`
   margin-top: 80px;
 
-  .title {
-    margin: 0 0 24px 120px;
-    color: #f7f7f7;
-    font-size: 48px;
+  section {
+    .title {
+      margin: 0 0 24px 120px;
+      color: #f7f7f7;
+      font-size: 54px;
+    }
+
+    .classes {
+      display: flex;
+      flex-wrap: wrap;
+    }
   }
 `;
 
@@ -27,16 +34,22 @@ export default function ClassesPage() {
       <section className='nearby-popular'>
         <h1 className='title'>Nearby Popular</h1>
         {/* Testing */}
-        <ClassCard classInfo={classData} />
+        <div className='classes'>
+          <ClassCard classInfo={classData} />
+          <ClassCard classInfo={classData} />
+          <ClassCard classInfo={classData} />
+          <ClassCard classInfo={classData} />
+          <ClassCard classInfo={classData} />
+        </div>
       </section>
 
-      <section className='preference'>
+      {/* <section className='preference'>
         <h1 className='title'>Based on Preference</h1>
-      </section>
+      </section> */}
 
-      <section className='new-classes'>
+      {/* <section className='new-classes'>
         <h1 className='title'>New Classes Near you</h1>
-      </section>
+      </section>{' '} */}
     </ClassesContainer>
   );
 }
