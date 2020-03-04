a4a06a1f628f0085becb9d3d7b3ea28bee0062de
rabah babaci <4rabah@gmail.com>
2020-03-04 15:10:14 -0800

    define the class card component for an Instructor

--- anywhere-fitness/src/ReusableComponents/InstructorClassCard.js
+++ anywhere-fitness/src/ReusableComponents/InstructorClassCard.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default function InstructorClassCard() {
-  return <div></div>;
+export default function InstructorClassCard({ classInfo }) {
+  return (
+    <div className='class-card'>
+      <img />
+      <h3>{classInfo.name}</h3>
+      <h2>{classInfo.location}</h2>
+      <p>{classInfo.teacher}</p>
+    </div>
+  );
 }
