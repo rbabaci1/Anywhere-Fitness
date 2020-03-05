97fbf2d83bb69620211d57368b7603f43b1486b4
rabah babaci <4rabah@gmail.com>
2020-03-04 16:14:01 -0800

    add state to AddNewClass and a changeHandler

--- anywhere-fitness/src/Pages/AddNewClassPage/AddNewClass.js
+++ anywhere-fitness/src/Pages/AddNewClassPage/AddNewClass.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Button from '../../ReusableComponents/Button';
 
 export default function AddNewClass() {
+  const [newClass, setNewClass] = useState({
+    name: '',
+    category: '',
+    location: '',
+    imageUrl: ''
+  });
+
+  const changeHandler = event => {
+    const { value, name } = event.target;
+
+    setNewClass({ ...newClass, [name]: value });
+  };
+
   return (
     <div>
-      <h1>Add a new Class</h1>
+      <h2>Add a new Class</h2>
+
+      <form>
+        <label>
+          <input type='text' name='' onChange={changeHandler} />
+        </label>
+
+        <label>
+          <input type='text' name='' onChange={changeHandler} />
+        </label>
+
+        <label>
+          <input type='text' name='' onChange={changeHandler} />
+        </label>
+
+        <label>
+          <input type='text' name='' onChange={changeHandler} />
+        </label>
+      </form>
     </div>
   );
 }
