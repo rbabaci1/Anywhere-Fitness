74ae194973cafcea6d15dcd63cc0af3cac2176b2
rabah babaci <4rabah@gmail.com>
2020-04-30 11:13:23 -0700

    use HatchRouter instead of browserRouter for browser refreshing purposes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,7 +14,7 @@ const store = createStore(reducer);
 
 function reducer() {
   return {
-    title: 'Hi from the Redux Store!'
+    title: 'Hi from the Redux Store!',
   };
 }
 
